42d600960b040f7221b524b6b1324bbccfdf75e3
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-05-31 11:36:19 +0200

    logik skola

---
+++ logik-2/attempt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logik_2
+{
+    public class attempt
+    {
+        public pin[] Pins { get; internal set; }
+
+        public attempt()
+        {
+            Pins = new pin[4];
+        }
+
+        public List<bool> Evaluate(pin[] pattern)
+        {
+            List<bool> result = new List<bool>();
+            List<pin> patternCopy = pattern.ToList();
+            int i = 0;
+            while (i < patternCopy.Count)
+            {
+                if(Pins[i] == patternCopy[i])
+                {
+                    result.Add(true);
+                    patternCopy.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return new List<bool>();
+        }
+
+    }
+    
+}
