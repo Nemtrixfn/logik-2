ba490381e9e0d32c1ad543a01ae3b4d92421eb37
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-05-31 11:08:24 +0200

    Přidejte soubory projektu.

---
+++ logik-2/pin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+namespace logik_2
+{
+    public class pin
+    {
+        public Color Color { get; set; }
+    }
+
+    public static class PinColor
+    {
+       public static Color Color1 = Colors.Red;
+       public static Color Color2 = Colors.Green;
+       public static Color Color3 = Colors.Blue;
+       public static Color Color4 = Colors.Orange;
+       public static Color Color5 = Colors.Purple;
+       public static Color Color6 = Colors.Yellow;
+    }
+}
