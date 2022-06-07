f1cca945fa705d314aad7b0546f5f27734555dd7
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-06-07 11:37:14 +0200

    pesek

--- logik-2/attempt.cs
+++ logik-2/attempt.cs
@@ -18,20 +18,36 @@ namespace logik_2
         public List<bool> Evaluate(pin[] pattern)
         {
             List<bool> result = new List<bool>();
-            List<pin> patternCopy = pattern.ToList();
-            int i = 0;
-            while (i < patternCopy.Count)
+            bool[] flag = new bool[4];
+
+            for (int i = 0; i < pattern.Length; i++)
             {
-                if(Pins[i] == patternCopy[i])
+                if(pattern[i] == Pins[i])
                 {
+                    flag[i] = false;
                     result.Add(true);
-                    patternCopy.RemoveAt(i);
-                }
-                else
+                }               
+            }
+            //flag[i] = pattern[i] != Pins[i];
+
+            for(int i = 0; i < flag.Length; i++)
+            {
+                if(flag[i])
                 {
-                    i++;
+                    for( int j = 0; j < pattern.Length; j++)
+                    {
+                        if(flag[j])
+                        {
+                            if(Pins[i] == pattern[j])
+                            {
+                                result.Add(false);
+                                break;
+                            }
+                        }
+                    }
                 }
             }
+            
 
             return new List<bool>();
         }
